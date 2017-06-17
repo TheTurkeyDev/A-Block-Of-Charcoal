adbb463561f9635bd11bc65565ab0066d2694640
Turkey <turkey2349@gmail.com>
2017-06-17 13:29:24 -0400

    Update to 1.10.2

--- src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
+++ src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
@@ -2,26 +2,36 @@ package com.turkey.charcoalBlock;
 
 import org.apache.logging.log4j.Logger;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.Block;
+import com.theprogrammingturkey.gobblecore.IModCore;
+import com.theprogrammingturkey.gobblecore.blocks.BaseBlock;
+import com.theprogrammingturkey.gobblecore.blocks.BlockLoader;
+import com.theprogrammingturkey.gobblecore.blocks.BlockManager;
+import com.theprogrammingturkey.gobblecore.blocks.IBlockHandler;
+import com.theprogrammingturkey.gobblecore.managers.CraftingManager;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemModelMesher;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.IFuelHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = CharcoalBlockCore.MODID, version = CharcoalBlockCore.VERSION, name = CharcoalBlockCore.Name)
-public class CharcoalBlockCore
+@Mod(modid = CharcoalBlockCore.MODID, version = CharcoalBlockCore.VERSION, name = CharcoalBlockCore.Name, dependencies = "after:gobblecore[0.1.4.13,)")
+public class CharcoalBlockCore implements IModCore
 {
-	public static final String MODID = "charcoalBlock";
+	public static final String MODID = "charcoalblock";
 	public static final String Name = "A Block of Charcoal";
 	public static final String VERSION = "1.0";
 
 	public static Logger logger;
 
-	public static Block theBlock;
+	public static BaseBlock theBlock;
 
 	public static CreativeTabs baseModTab = new CreativeTabs(MODID)
 	{
@@ -31,16 +41,65 @@ public class CharcoalBlockCore
 		}
 	};
 
+	public CharcoalBlockCore()
+	{
+		BlockManager.registerBlockHandler(new IBlockHandler()
+		{
+			@Override
+			public void registerBlocks(BlockLoader loader)
+			{
+				loader.setCreativeTab(baseModTab);
+				loader.registerBlock(theBlock = new BaseBlock("charcoal_block", 5), theBlock.getBlockName());
+			}
+
+			@Override
+			public void registerModels(BlockLoader loader)
+			{
+				ItemModelMesher mesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
+
+				loader.registerBlockModel(mesher, theBlock, 0, theBlock.getBlockName());
+			}
+		}, this);
+	}
+
 	@EventHandler
 	public void load(FMLPreInitializationEvent event)
 	{
 		logger = event.getModLog();
+	}
 
-		GameRegistry.registerBlock(theBlock = new CharcoalBlock().setBlockTextureName("charcoalBlock:CharcoalBlock"), "Charcoal_Block");
+	@EventHandler
+	public void load(FMLInitializationEvent event)
+	{
+		GameRegistry.registerFuelHandler(new IFuelHandler()
+		{
+			@Override
+			public int getBurnTime(ItemStack fuel)
+			{
+				if(Item.getItemFromBlock(CharcoalBlockCore.theBlock) == fuel.getItem())
+					return 16000;
+				return 0;
+			}
+		});
 
-		GameRegistry.registerFuelHandler(new FuelHandler());
-		
-		GameRegistry.addRecipe(new ItemStack(theBlock, 1), "AAA", "AAA", "AAA", 'A', new ItemStack(Items.coal, 1, 1));
-		GameRegistry.addShapelessRecipe(new ItemStack(Items.coal, 9, 1), new ItemStack(theBlock, 1));
+		CraftingManager.register3x3CompressionRecipe(new ItemStack(Items.COAL, 1, 1), new ItemStack(theBlock, 1), true);
+	}
+
+	@Override
+	public String getModID()
+	{
+		return MODID;
+	}
+
+	@Override
+	public String getName()
+	{
+		return Name;
+	}
+
+	@Override
+	public String getVersion()
+	{
+		return VERSION;
 	}
 }
