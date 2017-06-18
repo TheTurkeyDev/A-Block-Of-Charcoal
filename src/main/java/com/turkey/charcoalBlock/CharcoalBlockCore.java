82922a3298edfa29989e1b4c21d064377e03824f
Turkey <turkey2349@gmail.com>
2017-06-18 18:58:33 -0400

    Fixed a crash with Gobblecore

--- src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
+++ src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
@@ -25,12 +25,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = CharcoalBlockCore.MODID, version = CharcoalBlockCore.VERSION, name = CharcoalBlockCore.Name, dependencies = "after:gobblecore[0.1.4.14,)")
+@Mod(modid = CharcoalBlockCore.MODID, version = CharcoalBlockCore.VERSION, name = CharcoalBlockCore.Name, dependencies = "required-after:gobblecore[0.1.4.14,)")
 public class CharcoalBlockCore implements IModCore
 {
 	public static final String MODID = "charcoalblock";
 	public static final String Name = "A Block of Charcoal";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	public static Logger logger;
 
