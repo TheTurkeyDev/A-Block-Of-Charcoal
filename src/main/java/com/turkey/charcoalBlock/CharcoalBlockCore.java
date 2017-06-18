6beb02150a321280f55d9f50584cd7e996962a66
Turkey <turkey2349@gmail.com>
2017-06-18 18:59:32 -0400

    Updated versions

--- src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
+++ src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
@@ -30,7 +30,7 @@ public class CharcoalBlockCore implements IModCore
 {
 	public static final String MODID = "charcoalblock";
 	public static final String Name = "A Block of Charcoal";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 
 	public static Logger logger;
 
