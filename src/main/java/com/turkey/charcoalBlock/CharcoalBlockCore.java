1bfaca040bc7abcc1fc9a21f8680d030e5488bbd
Turkey <turkey2349@gmail.com>
2018-06-18 14:45:22 -0400

    Removed dependency on GobbleCore

--- src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
+++ src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
@@ -1,103 +1,45 @@
 package com.turkey.charcoalBlock;
 
-import org.apache.logging.log4j.Logger;
-
-import com.google.gson.JsonElement;
-import com.theprogrammingturkey.gobblecore.IModCore;
-import com.theprogrammingturkey.gobblecore.blocks.BaseBlock;
-import com.theprogrammingturkey.gobblecore.blocks.BlockLoader;
-import com.theprogrammingturkey.gobblecore.blocks.BlockManager;
-import com.theprogrammingturkey.gobblecore.blocks.IBlockHandler;
-import com.theprogrammingturkey.gobblecore.items.IItemHandler;
-import com.theprogrammingturkey.gobblecore.items.ItemLoader;
-import com.theprogrammingturkey.gobblecore.items.ItemManager;
-import com.theprogrammingturkey.gobblecore.managers.CraftingManager;
-import com.theprogrammingturkey.gobblecore.managers.WebHookManager;
-import com.theprogrammingturkey.gobblecore.managers.WebHookManager.ModWebHook;
-
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemModelMesher;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.IFuelHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = CharcoalBlockCore.MODID, version = CharcoalBlockCore.VERSION, name = CharcoalBlockCore.Name, dependencies = "required-after:gobblecore")
-public class CharcoalBlockCore implements IModCore
+@Mod(modid = CharcoalBlockCore.MODID, version = CharcoalBlockCore.VERSION, name = CharcoalBlockCore.NAME)
+public class CharcoalBlockCore
 {
 	public static final String MODID = "charcoalblock";
-	public static final String Name = "A Block of Charcoal";
+	public static final String NAME = "A Block of Charcoal";
 	public static final String VERSION = "1.1";
 
-	public static Logger logger;
-
-	public static BaseBlock theBlock;
-
-	public static CreativeTabs baseModTab = new CreativeTabs(MODID)
-	{
-		public ItemStack getTabIconItem()
-		{
-			return new ItemStack(theBlock);
-		}
-	};
-
-	public CharcoalBlockCore()
-	{
-		BlockManager.registerBlockHandler(new IBlockHandler()
-		{
-			@Override
-			public void registerBlocks(BlockLoader loader)
-			{
-				loader.setCreativeTab(baseModTab);
-				loader.registerBlock(theBlock = new BaseBlock("charcoal_block", 5), theBlock.getBlockName());
-			}
-
-			@Override
-			public void registerModels(BlockLoader loader)
-			{
-				ItemModelMesher mesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
-
-				loader.registerBlockModel(mesher, theBlock, 0, theBlock.getBlockName());
-			}
-		}, this);
-
-		ItemManager.registerItemHandler(new IItemHandler()
-		{
-
-			@Override
-			public void registerItems(ItemLoader loader)
-			{
-				loader.setCreativeTab(baseModTab);
-				loader.registerBlockItem(theBlock);
-			}
-
-			@Override
-			public void registerModels(ItemLoader loader)
-			{
-
-			}
-
-		}, this);
-	}
+	public static Block theBlock;
 
 	@EventHandler
 	public void load(FMLPreInitializationEvent event)
 	{
-		logger = event.getModLog();
-
-		WebHookManager.registerHook(new ModWebHook(this)
+		if(event.getSide() == Side.CLIENT)
 		{
-			@Override
-			public void onResponse(JsonElement json)
-			{
-			}
-		});
+			ItemModelMesher mesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
+
+			mesher.register(Item.getItemFromBlock(theBlock), 0, new ModelResourceLocation(MODID + ":charcoal_block", "inventory"));
+		}
 	}
 
 	@EventHandler
@@ -114,24 +56,26 @@ public class CharcoalBlockCore implements IModCore
 			}
 		});
 
-		CraftingManager.register3x3CompressionRecipe(new ItemStack(Items.COAL, 1, 1), new ItemStack(theBlock, 1), true);
+		ResourceLocation group = new ResourceLocation(NAME);
+		GameRegistry.addShapedRecipe(new ResourceLocation(MODID, "Charcol Block Compression"), group, new ItemStack(theBlock, 1), "III", "III", "III", 'I', new ItemStack(Items.COAL, 1, 1));
+		GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "Charcol Block Uncompression"), group, new ItemStack(Items.COAL, 9, 1), Ingredient.func_193369_a(new ItemStack(theBlock, 1)));
 	}
 
-	@Override
-	public String getModID()
+	@SubscribeEvent
+	public void onBlockRegistry(RegistryEvent.Register<Block> e)
 	{
-		return MODID;
+		theBlock = new Block(Material.GROUND);
+		theBlock.setCreativeTab(CreativeTabs.MATERIALS);
+		theBlock.setHardness(5);
+		theBlock.setUnlocalizedName("charcoal_block");
+		e.getRegistry().register(theBlock);
 	}
 
-	@Override
-	public String getName()
+	@SubscribeEvent
+	public void onItemRegistry(RegistryEvent.Register<Item> e)
 	{
-		return Name;
-	}
-
-	@Override
-	public String getVersion()
-	{
-		return VERSION;
+		ItemBlock ib = new ItemBlock(theBlock);
+		ib.setRegistryName(theBlock.getRegistryName());
+		e.getRegistry().register(ib);
 	}
 }
