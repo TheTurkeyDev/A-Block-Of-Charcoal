d58b1f0d6b48a92bb89d3716a63df70e93892914
Turkey2349 <turkey2349@gmail.com>
2019-03-04 17:07:28 -0500

    Porting to 1.13.2

--- src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
+++ src/main/java/com/turkey/charcoalBlock/CharcoalBlockCore.java
@@ -2,80 +2,49 @@ package com.turkey.charcoalBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.IFuelHandler;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
 
-@Mod(modid = CharcoalBlockCore.MODID, version = CharcoalBlockCore.VERSION, name = CharcoalBlockCore.NAME)
+@Mod(CharcoalBlockCore.MODID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CharcoalBlockCore
 {
 	public static final String MODID = "charcoalblock";
-	public static final String NAME = "A Block of Charcoal";
-	public static final String VERSION = "1.1";
 
 	public static Block theBlock;
+	public static Item theItemBlock;
 
-	@EventHandler
-	public void preinit(FMLPreInitializationEvent event)
+	public CharcoalBlockCore()
 	{
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
-	@EventHandler
-	public void init(FMLInitializationEvent event)
+	@SubscribeEvent
+	public void fuelEvent(FurnaceFuelBurnTimeEvent event)
 	{
-		if(event.getSide() == Side.CLIENT)
-		{
-			Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(theBlock), 0, new ModelResourceLocation(MODID + ":charcoal_block", "inventory"));
-		}
-		GameRegistry.registerFuelHandler(new IFuelHandler()
-		{
-			@Override
-			public int getBurnTime(ItemStack fuel)
-			{
-				if(Item.getItemFromBlock(CharcoalBlockCore.theBlock) == fuel.getItem())
-					return 16000;
-				return 0;
-			}
-		});
-
-		ResourceLocation group = new ResourceLocation(NAME);
-		GameRegistry.addShapedRecipe(new ResourceLocation(MODID, "Charcol Block Compression"), group, new ItemStack(theBlock, 1), "III", "III", "III", 'I', new ItemStack(Items.COAL, 1, 1));
-		GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "Charcol Block Uncompression"), group, new ItemStack(Items.COAL, 9, 1), Ingredient.func_193369_a(new ItemStack(theBlock, 1)));
+		if(event.getItemStack().getItem().equals(theItemBlock))
+			event.setBurnTime(16000);
 	}
 
 	@SubscribeEvent
 	public void onBlockRegistry(RegistryEvent.Register<Block> e)
 	{
-		theBlock = new Block(Material.GROUND);
-		theBlock.setCreativeTab(CreativeTabs.MATERIALS);
-		theBlock.setHardness(5);
+		theBlock = new Block(Block.Properties.create(Material.GROUND).hardnessAndResistance(5));
 		theBlock.setRegistryName(MODID, "charcoal_block");
-		theBlock.setUnlocalizedName("charcoal_block");
 		e.getRegistry().register(theBlock);
 	}
 
 	@SubscribeEvent
 	public void onItemRegistry(RegistryEvent.Register<Item> e)
 	{
-		ItemBlock ib = new ItemBlock(theBlock);
-		ib.setRegistryName(theBlock.getRegistryName());
-		e.getRegistry().register(ib);
+		theItemBlock = new ItemBlock(theBlock, (new Item.Properties()).group(ItemGroup.MATERIALS));
+		theItemBlock.setRegistryName(theBlock.getRegistryName());
+		e.getRegistry().register(theItemBlock);
 	}
 }
