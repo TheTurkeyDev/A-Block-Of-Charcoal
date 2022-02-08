5ca2642fe57d04a265b84622633fb24681806306
TheTurkeyDev <turkey@theturkey.dev>
2022-02-07 22:51:36 -0500

    Ported to 1.18

--- src/main/java/dev/theturkey/charcoalBlock/CharcoalBlockCore.java
+++ src/main/java/dev/theturkey/charcoalBlock/CharcoalBlockCore.java
@@ -1,16 +1,19 @@
-package com.turkey.charcoalBlock;
+package dev.theturkey.charcoalBlock;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MaterialColor;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nullable;
 
 @Mod(CharcoalBlockCore.MODID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -28,7 +31,7 @@ public class CharcoalBlockCore
 	@SubscribeEvent
 	public static void onBlockRegistry(RegistryEvent.Register<Block> e)
 	{
-		theBlock = new Block(Block.Properties.create(Material.ROCK, MaterialColor.BLACK).hardnessAndResistance(5.0F, 6.0F));
+		theBlock = new Block(BlockBehaviour.Properties.of(Material.DIRT).strength(5.0F, 6.0F));
 		theBlock.setRegistryName(MODID, "charcoal_block");
 		e.getRegistry().register(theBlock);
 	}
@@ -36,10 +39,10 @@ public class CharcoalBlockCore
 	@SubscribeEvent
 	public static void onItemRegistry(RegistryEvent.Register<Item> e)
 	{
-		Item theItemBlock = new BlockItem(theBlock, (new Item.Properties()).group(ItemGroup.BUILDING_BLOCKS))
+		Item theItemBlock = new BlockItem(theBlock, (new Item.Properties()).tab(CreativeModeTab.TAB_BUILDING_BLOCKS))
 		{
 			@Override
-			public int getBurnTime(ItemStack itemStack)
+			public int getBurnTime(ItemStack itemStack, @Nullable RecipeType<?> recipeType)
 			{
 				return 14400;
 			}
